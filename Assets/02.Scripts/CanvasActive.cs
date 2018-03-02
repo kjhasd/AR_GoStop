8096512da8cf36f9b6c253fea5e8af49e0be15f8
DESKTOP-GUUVPMM\kjhas <jhk1110kr@gmail.com>
2018-03-02 14:09:05 +0900

    01~06

---
+++ Assets/02.Scripts/CanvasActive.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasActive : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        transform.parent.GetChild(0).gameObject.SetActive(true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
